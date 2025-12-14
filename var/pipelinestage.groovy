4a4665a3f703627b59b5bbea86b36aac65c7edab
dilipdevopse <dilipdara2000@gmail.com>
2025-12-14 19:59:05 +0530

    Update pipelinestage.groovy

--- var/pipelinestage.groovy
+++ var/pipelinestage.groovy
@@ -1,17 +1,21 @@
 def call() {
+    git branch: 'dev',
+        credentialsId: '047e0051-21a4-419a-8217-f8c646580ad9',
+        url: 'https://github.com/dilipdevopsj/we.git'
+}
+def call() {
     sh 'mvn clean package'
 }
 def call() {
     sh 'mvn sonar:sonar'
 }
-
 def call() {
     sh 'mvn deploy'
 }
 def call() {
-         sh """
-             curl -u sai:dd \
-              --upload-file /var/lib/jenkins/workspace/WQ/target/maven-web-application.war \
-              "http://13.235.243.213:8080/manager/text/deploy?path=/maven-web-application&update=true"
-              """
-        }
+    sh """
+        curl -u sai:dd \
+        --upload-file target/maven-web-application.war \
+        "http://13.235.243.213:8080/manager/text/deploy?path=/maven-web-application&update=true"
+    """
+}
